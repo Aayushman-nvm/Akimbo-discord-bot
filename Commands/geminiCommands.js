b6f4617a9a0e3c796980d0c187067fc6fba54eb2
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-06-12 22:57:43 +0530

    sending channel id to store context in db

--- Commands/geminiCommands.js
+++ Commands/geminiCommands.js
@@ -15,14 +15,14 @@ export const gemini = new SlashCommandBuilder()
 
 export async function askGemini(interaction) {
   const prompt = interaction.options.getString("prompt");
-  console.log(`Gemini Prompt: ${prompt}no`);
+  console.log(`Gemini Prompt: ${prompt}`);
 
   try {
     await interaction.deferReply();
 
     await interaction.followUp(`Prompt by ${interaction.user}: ${prompt}\n`);
 
-    const response = await geminiServices(prompt);
+    const response = await geminiServices(prompt, interaction.channelId);
     console.log(`Gemini Response: ${response}`);
 
     const formatted = formatResponse(response);
